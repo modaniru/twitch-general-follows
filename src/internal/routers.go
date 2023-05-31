992e99501ebc106cd6dd646d32eb1fb8bd500d22
aleksejmoksancev <mokshantsev.aleksei>
2023-05-31 23:09:22 +0500

    add test endpoint uri for twtich api and client_id/secret in config.toml modified:   config.toml modified:   go.mod modified:   go.sum modified:   src/internal/routers.go modified:   src/internal/server.go

--- src/internal/routers.go
+++ src/internal/routers.go
@@ -1,7 +1,39 @@
 package internal
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"time"
 
-func ping(c *gin.Context){
+	"github.com/gin-gonic/gin"
+	"github.com/gojek/heimdall/v7/httpclient"
+)
+
+type post struct{
+	Id int `json:"id"`
+	UserId int `json:"userId"`
+	Title string `json:"title"`
+	Body string `json:"body"`
+}
+
+func ping(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "ping"})
+}
+
+func testClient(c *gin.Context) {
+	// Create a new HTTP client with a default timeout
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))
+
+	// Use the clients GET method to create and execute the request
+	res, err := client.Get("https://jsonplaceholder.typicode.com/posts", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Heimdall returns the standard *http.Response object
+	body, err := ioutil.ReadAll(res.Body)
+	var posts []post
+	json.Unmarshal([]byte(string(body)), &posts)
+	c.JSON(200, posts)
 }
--- src/internal/server.go
+++ src/internal/server.go
@@ -2,20 +2,21 @@ package internal
 
 import "github.com/gin-gonic/gin"
 
-type server struct{
+type server struct {
 	Config *configauration
 	Router *gin.Engine
 }
 
-func NewServer(configPath string) *server{
+func NewServer(configPath string) *server {
 	return &server{NewConfiguration(configPath), gin.Default()}
 }
 
-func (s *server)Start(){
+func (s *server) Start() {
 	s.initRouters()
 	s.Router.Run(s.Config.Port)
 }
 
-func (s *server)initRouters(){
+func (s *server) initRouters() {
 	s.Router.GET("/ping", ping)
+	s.Router.GET("/testing/testclient", testClient)
 }
